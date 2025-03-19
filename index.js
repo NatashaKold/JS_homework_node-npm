6a338619ddaffe88baff8da03e9c82b43b5f6184
NatashaKold <n.koldunova@gmail.com>
2025-03-19 07:03:40 +0300

    planet search added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-http ('swapi.py4e.com/api/planets/1/')
+async function getItem(){
+    const result = document.querySelector(".resultContainer");
+    let index = document.getElementById('indexInput').value;
+    const category = document.getElementById('categoriesSelect').value;
+
+if(category==='planets'){
+await fetch (`https://swapi.py4e.com/api/planets/${index}`)
+.then(response => response.json())
+.then((data)=>{
+result.innerHTML = `<div style='font-size: 30px'>Планета: </div>
+                    <div style='font-size: 30px'>${JSON.stringify(data.name)}</div>`
+})
+.catch((error)=>{
+    result.innerHTML=`<div style='font-size: 30px'> Произошла ошибка: ${error.message} 🙁 </div>`
+})
+}
+}
