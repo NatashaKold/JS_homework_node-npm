b637d56d5e60b5800343b3285c7b02a04dbe0b68
NatashaKold <n.koldunova@gmail.com>
2025-03-22 11:36:27 +0300

    js corrections made

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const button = document.getElementById('searchBtn');
 let result = document.getElementById('response');
 let errorMessage = document.getElementById('errorMessage');
-let index = document.getElementById('indexInput').value;
-const category = document.getElementById('categoriesSelect').value;
-
 
 async function getItem(){
     result.textContent = '';
     errorMessage.textContent='';
 
-if(category==='planets'){
+    let index = document.getElementById('indexInput').value;
+    const category = document.getElementById('categoriesSelect').value;
+
+if(!index){
+    errorMessage.textContent = 'Введите номер от 1 до 10!';
+    return;
+}
+
+const url = `https://swapi.py4e.com/api/${category}/${index}`;
+
 try{
- await fetch (`https://swapi.py4e.com/api/planets/${index}`)
-.then(response => response.json())
-.then((data)=>{
-result.textContent = `Планета: ${JSON.stringify(data.name)}`;
-})
+ const response = await fetch (url);
+ if(!response.ok){
+    throw new Error("Не удалось загрузить данные!");
+ }
+ const data = await response.json();
+
+let categoryName = category ==='planets'? 'Планета' :
+category ==='starships'?'Космический корабль':
+category ==='people'?'Персонаж': 'Объект';
+
+ if(!data.name){
+    throw new Error(`Данные об объекте ${categoryName} не найдены`);
+ }
+
+result.textContent = `${categoryName}: ${data.name}`;
 } catch(error){
     console.log(error);
     errorMessage.textContent= `Произошла ошибка: ${error.message} 🙁`;
@@ -24,38 +40,4 @@ result.textContent = `Планета: ${JSON.stringify(data.name)}`;
 }
 }
 
-if(category==='starships'){
-try{
-    await fetch (`https://swapi.py4e.com/api/starships/${index}`)
-    .then(response => response.json())
-    .then((data)=>{
-    result.textContent = `Космический корабль: ${JSON.stringify(data.name)}`;
-    })
-}
-catch(error){
-    console.log(error);
-        errorMessage.textContent=` Произошла ошибка: ${error.message} 🙁`;
-    } finally{
-        document.querySelector('resultContainer').textContent = `StarWars - лучшая сага в мире кино!`;
-    }
-}
-
-
-    if(category==='people'){
-    try{
-    await fetch (`https://swapi.py4e.com/api/people/${index}`)
-    .then(response => response.json())
-    .then((data)=>{
-    result.textContent = `Персонаж: ${JSON.stringify(data.name)}`;
-    })
-    } catch(error){
-        console.log(error);
-        errorMessage.textContent=`Произошла ошибка: ${error.message} 🙁`;
-    }finally{
-        document.querySelector('resultContainer').textContent = `StarWars - лучшая сага в мире кино!`;
-    }
-}
-        
-}
-
-button.addEventListener('click', getItem())
+button.addEventListener('click', getItem);
